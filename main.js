16f5abf773b0ac11316b4e735e38586f4a39a98f
wingillis <win.gillis@gmail.com>
2017-09-02 21:01:04 -0400

    Added a nice search functionality

--- main.js
+++ main.js
@@ -8,13 +8,26 @@ const imageHandler = require('./lib/images');
 const uuid = require('uuid/v4'); // random uuid
 const Editor = require('./lib/editor');
 const SearchBar = require('./lib/search');
-// const {ipcRenderer} = require('electron');
+const {remote} = require('electron');
+let menu = remote.Menu.getApplicationMenu();
 
 // singleton
 const db = require('./lib/db');
 
 let editor = new Editor('code');
-let searchBar = new SearchBar('sidebar');
-// db.getNewestFile().then(function (resp) {
-  // editor.setFile(resp)
-// });
+let searchBar = new SearchBar('sidebar', editor);
+
+
+menu.items[1].submenu.append(new remote.MenuItem({
+  label: 'New File',
+  accelerator: 'CmdOrCtrl+N',
+  click: () => {editor.newFile()}
+}));
+
+menu.items[1].submenu.append(new remote.MenuItem({
+  label: 'Find Entry',
+  accelerator: 'CmdOrCtrl+L',
+  click: () => {searchBar.focusSearch()}
+}))
+
+remote.Menu.setApplicationMenu(menu);
